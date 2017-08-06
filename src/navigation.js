654c685e8004c5312fc5718e0169ce189f29c094
mysterion <sv.hornung@gmx.de>
2017-08-06 23:31:33 +0200

    added new logo and favicon replaced default react header with own header

--- src/navigation.js
+++ src/navigation.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react';
 import {Navbar, Nav, NavItem} from 'react-bootstrap';
+import './navigation.css'
+
 
 class Navigation extends Component {
   render() {
     return (
       <div className='Navigation'>
         <Navbar inverse collapseOnSelect>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <a>Blog-App</a>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-          </Navbar.Header>
           <Navbar.Collapse>
             <Nav>
               <NavItem eventKey={1}>Home</NavItem>
